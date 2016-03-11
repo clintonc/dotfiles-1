ff28416815463f160728fe3e322d10252964d727
Matthew L. Curry <mlcurry@sandia.gov>
2016-03-11 15:53:54 -0700

    Add functions for dns flushing and untracked file listing

---
+++ bash_include.sh
@@ -0,0 +1,8 @@
+mlc_git_list_untracked() {
+    git status --porcelain --ignored | grep '^!!' | awk '{print $2}'
+}
+
+mlc_flush_dns() {
+    dscacheutil -flushcache
+    sudo killall -HUP mDNSResponder
+}
